710b0c0f99848515162653e671c079f39d15adb5
marcii-101 <86745494+marcii-101@users.noreply.github.com>
2022-06-11 19:56:22 +0200

    Update cvm-neocities.sh

--- cvm-neocities.sh
+++ cvm-neocities.sh
@@ -3,7 +3,7 @@ set -e
 function doVagrant() {
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install -y vagrant
+sudo apt update && sudo apt install -y vagrant
 }
 function download() {
 cd ~
